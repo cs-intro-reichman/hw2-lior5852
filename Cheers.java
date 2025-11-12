1e14c8869f5ecbb4ba6474d5f687e93074403494
Lior Tzur <liortzur@Liors-MacBook-Air.local>
2025-11-12 20:29:47 +0200

    fixed spacing for autograder

--- Cheers.java
+++ Cheers.java
@@ -7,33 +7,35 @@ Great work boaz!
 
 // Prints a crowd cheering output.
 // Prints a crowd cheering output.
+// Prints a crowd cheering output.
 public class Cheers {
     public static void main(String[] args) {
         String word = args[0];
         int times = Integer.parseInt(args[1]);
 
-        // אותיות שמקבלות "an"
+        // אותיות שדורשות "an"
         String lettersWithAn = "AEFHILMNORSX";
 
-        // קטע ה-"Give me ..."
+        // מעבר על כל אות במילה
         for (int i = 0; i < word.length(); i++) {
             char upper = Character.toUpperCase(word.charAt(i));
             boolean useAn = (lettersWithAn.indexOf(upper) != -1);
 
             if (useAn) {
-                System.out.println("Give me an " + upper + ": " + upper + "!");
+                // 👇 רווח כפול אחרי הנקודתיים
+                System.out.println("Give me an " + upper + ":  " + upper + "!");
             } else {
-                System.out.println("Give me a " + upper + ": " + upper + "!");
+                System.out.println("Give me a " + upper + ":  " + upper + "!");
             }
         }
 
-        // שורה ריקה אחת בלבד לפני השאלה
+        // שורה ריקה אחת
         System.out.println();
 
         // השאלה
         System.out.println("What does that spell?");
 
-        // ההרשאות של המילה באותיות גדולות
+        // הדפסות חוזרות של המילה באותיות גדולות
         String shout = word.toUpperCase();
         for (int j = 0; j < times; j++) {
             System.out.println(shout + "!!!");
