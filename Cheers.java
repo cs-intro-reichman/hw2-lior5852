2c2b4e0468bb48c9a14ceccde38c3441ddb45df4
Lior Tzur <liortzur@Liors-MacBook-Air.local>
2025-11-12 20:21:32 +0200

    4real

--- Cheers.java
+++ Cheers.java
@@ -7,39 +7,35 @@ Great work boaz!
 
 // Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
-        // נקבל את הקלטים
+        public static void main(String[] args) {   // נקבל את הקלטים
         String word = args[0];
         int times = Integer.parseInt(args[1]);
 
-        // רשימת אותיות שדורשות "an"
+        // רשימת האותיות שדורשות "an"
         String lettersWithAn = "AEFHILMNORSX";
 
-        // לולאה שעוברת על כל אות במילה
-        int i = 0;
-        while (i < word.length()) {
+        // נריץ לולאה על כל אות
+        for (int i = 0; i < word.length(); i++) {
             char current = word.charAt(i);
-            char upper = Character.toUpperCase(current); // כדי לבדוק אם ב-AEFH...
+            char upper = Character.toUpperCase(current);
 
             if (lettersWithAn.indexOf(upper) != -1) {
                 System.out.println("Give me an " + upper + ": " + upper + "!");
             } else {
                 System.out.println("Give me a " + upper + ": " + upper + "!");
             }
-
-            i = i + 1;
         }
 
-        // שורה ריקה לפני המשפט
+        // שורה ריקה אחת
         System.out.println();
-        System.out.println("What does that spell?");
 
-        // לולאה שחוזרת על המילה באותיות גדולות
-        int j = 0;
-        while (j < times) {
+        // המשפט
+        System.out.println("What does that spell?");
+        // אין שורה ריקה אחרי זה!
+
+        // לולאה לחזרות
+        for (int j = 0; j < times; j++) {
             System.out.println(word.toUpperCase() + "!!!");
-            j = j + 1;
-        }
-        
+        }   
         }
 }
