1f0a7cfc31c20205c4897dcd19e9713708260a6b
Lior Tzur <liortzur@Liors-MacBook-Air.local>
2025-11-12 22:27:43 +0200

    final full fix - correct 'an' and 'U' spacing

--- Cheers.java
+++ Cheers.java
@@ -21,31 +21,38 @@ Great work boaz!
 // Prints a crowd cheering output.
 // Prints a crowd cheering output.
 // Prints a crowd cheering output.
+// Prints a crowd cheering output.
 public class Cheers {
     public static void main(String[] args) {
         String word = args[0];
         int times = Integer.parseInt(args[1]);
-        String upperWord = word.toUpperCase();
 
-        // אותיות שדורשות "an"
-        String lettersWithAn = "AEFHILMNORSX";
+        String upperWord = word.toUpperCase();
+        String anLetters = "AEFHILMNORSX";
 
         for (int i = 0; i < upperWord.length(); i++) {
-            char letter = upperWord.charAt(i);
+            char ch = upperWord.charAt(i);
 
-            if (lettersWithAn.indexOf(letter) != -1) {
-                System.out.println("Give me an " + letter + ": " + letter + "!");
-            } else if (letter == 'U') {
-                System.out.println("Give me a  " + letter + ": " + letter + "!");
+            String article;
+            if (anLetters.indexOf(ch) != -1) {
+                article = "an";
             } else {
-                System.out.println("Give me a " + letter + ": " + letter + "!");
+                // special-case: "a  U" (two spaces before U)
+                article = (ch == 'U') ? "a " : "a";
+            }
+
+            // build the "Give me ..." line
+            if (ch == 'U' && article.equals("a ")) {
+                // exactly: "Give me a  U: U!"
+                System.out.println("Give me a  " + ch + ": " + ch + "!");
+            } else {
+                System.out.println("Give me " + article + " " + ch + ": " + ch + "!");
             }
         }
 
         System.out.println("What does that spell?");
 
-        // הדפסות לפי times
-        for (int j = 0; j < times; j++) {
+        for (int i = 0; i < times; i++) {
             System.out.println(upperWord + "!!!");
         }
     }
