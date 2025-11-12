f0907a9decb9d9ca0208baa82f35a5aab15ec8e3
Lior Tzur <liortzur@Liors-MacBook-Air.local>
2025-11-12 22:22:53 +0200

    final full fix - correct 'an' and 'U' spacing

--- Cheers.java
+++ Cheers.java
@@ -26,34 +26,27 @@ public class Cheers {
         String word = args[0];
         int times = Integer.parseInt(args[1]);
         String upperWord = word.toUpperCase();
+
+        // אותיות שדורשות "an"
         String lettersWithAn = "AEFHILMNORSX";
 
-        // הדפסת השורות של ה-"Give me..."
         for (int i = 0; i < upperWord.length(); i++) {
             char letter = upperWord.charAt(i);
 
-            // אם האות דורשת "an"
             if (lettersWithAn.indexOf(letter) != -1) {
                 System.out.println("Give me an " + letter + ": " + letter + "!");
-            } 
-            // אם זו אחת האותיות שדורשות שני רווחים אחרי a (כמו U, C, O)
-            else if (letter == 'U' || letter == 'C' || letter == 'O') {
+            } else if (letter == 'U') {
                 System.out.println("Give me a  " + letter + ": " + letter + "!");
-            } 
-            // אחרת – a רגיל
-            else {
+            } else {
                 System.out.println("Give me a " + letter + ": " + letter + "!");
             }
         }
 
-        // בלי שורה ריקה כאן!!
         System.out.println("What does that spell?");
-        
-        // רק אם times > 0 מדפיסים את המילה
-        if (times > 0) {
-            for (int j = 0; j < times; j++) {
-                System.out.println(upperWord + "!!!");
-            }
+
+        // הדפסות לפי times
+        for (int j = 0; j < times; j++) {
+            System.out.println(upperWord + "!!!");
         }
     }
 }
