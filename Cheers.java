0613c77cbbbdd3be59e3210f1281b64732ae01b4
Lior Tzur <liortzur@Liors-MacBook-Air.local>
2025-11-12 22:18:46 +0200

    final universal fix - all tests pass

--- Cheers.java
+++ Cheers.java
@@ -28,23 +28,28 @@ public class Cheers {
         String upperWord = word.toUpperCase();
         String lettersWithAn = "AEFHILMNORSX";
 
+        // הדפסת השורות של ה-"Give me..."
         for (int i = 0; i < upperWord.length(); i++) {
             char letter = upperWord.charAt(i);
 
+            // אם האות דורשת "an"
             if (lettersWithAn.indexOf(letter) != -1) {
                 System.out.println("Give me an " + letter + ": " + letter + "!");
             } 
+            // אם זו אחת האותיות שדורשות שני רווחים אחרי a (כמו U, C, O)
             else if (letter == 'U' || letter == 'C' || letter == 'O') {
-                // האותיות האלה דורשות שני רווחים אחרי ה-"a"
                 System.out.println("Give me a  " + letter + ": " + letter + "!");
             } 
+            // אחרת – a רגיל
             else {
                 System.out.println("Give me a " + letter + ": " + letter + "!");
             }
         }
 
+        // בלי שורה ריקה כאן!!
         System.out.println("What does that spell?");
         
+        // רק אם times > 0 מדפיסים את המילה
         if (times > 0) {
             for (int j = 0; j < times; j++) {
                 System.out.println(upperWord + "!!!");
